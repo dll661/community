7ebf6c90122f208317fb50d10237ca327dc8cf67
denglong <244007811@qq.com>
2019-05-30 23:39:16 +0800

    init

---
+++ src/main/java/life/com/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package life.com.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author : denglong
+ * @Description:
+ * @Date: Created 22:58 2019/5/30
+ * @Modified By:
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hell(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
